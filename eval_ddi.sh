7ae1f09a9eae65375da030ecdc791f9c7bf61225
Junlin Yao <jyao@cab-e81-13.inf.ethz.ch>
2017-10-24 16:27:41 +0200

    minor fix

--- eval_ddi.sh
+++ eval_ddi.sh
@@ -1,1 +1,8 @@
-python eval_wc.py --load_arg ./checkpoint/drugddi.modelcwlm_lstm_crf.json --load_check_point ./checkpoint/drugddi.modelcwlm_lstm_crf.model --gpu -1 --dev_file ../data/DrugDDI/test.ddi --test_file ../data/DrugDDI/test.ddi
+python eval_w.py --load_arg ./checkpoint/drugddi_lstm_crf.json --load_check_point ./checkpoint/drugddi_lstm_crf.model --gpu -1 --dev_file ../data/DrugDDI/test.ddi --test_file ../data/DrugDDI/test.ddi
+
+python eval_wc.py --load_arg ./checkpoint/drugddi2011_we_cwlm_lstm_crf.json --load_check_point ./checkpoint/drugddi2011_we_cwlm_lstm_crf.model --gpu -1 --dev_file ../data/drugddi2011/test.ddi --test_file ../data/drugddi2011/test.ddi
+
+python eval_wc.py --load_arg ./checkpoint/drugddi2013_we_cwlm_lstm_crf.json --load_check_point ./checkpoint/drugddi2013_we_cwlm_lstm_crf.model --gpu -1 --dev_file ../data/drugddi2013/test.ddi --test_file ../data/drugddi2013/test.ddi
+
+python eval_wc.py --load_arg ./checkpoint/drugddi2011_we_cwlm_lstm_crf.json --load_check_point ./checkpoint/drugddi2011_we_cwlm_lstm_crf.model --gpu -1 --dev_file ../data/ddi_tiny/test.ddi --test_file ../data/ddi_tiny/test.ddi
+
