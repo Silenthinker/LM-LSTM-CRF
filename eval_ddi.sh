7b53dac3d6f9f330e6194131d246c4cf51212fad
Silenthinker <sparklethinker@gmail.com>
2017-11-09 14:27:28 +0100

    uptodate

--- eval_ddi.sh
+++ eval_ddi.sh
@@ -1,6 +1,6 @@
 python eval_w.py --load_arg ./checkpoint/drugddi2011_1_lstm_crf.json --load_check_point ./checkpoint/drugddi2011_1_lstm_crf.model --gpu -1 --dev_file ../data/drugddi2011/test.ddi --test_file ../data/drugddi2011/test.ddi
 
-python eval_wc.py --load_arg ./checkpoint/drugddi2011_we_2_cwlm_lstm_crf.json --load_check_point ./checkpoint/drugddi2011_we_2_cwlm_lstm_crf.model --gpu -1 --dev_file ../data/drugddi2011/val.ddi --test_file ../data/drugddi2011/test.ddi
+python eval_wc.py --load_arg ./checkpoint/drugddi2011_we_1_cwlm_lstm_crf.json --load_check_point ./checkpoint/drugddi2011_we_1_cwlm_lstm_crf.model --gpu -1 --dev_file ../data/drugddi2011/val.ddi --test_file ../data/drugddi2011/test.ddi
 
 python eval_wc.py --load_arg ./checkpoint/drugddi2013_we_cwlm_lstm_crf.json --load_check_point ./checkpoint/drugddi2013_we_cwlm_lstm_crf.model --gpu -1 --dev_file ../data/drugddi2013/test.ddi --test_file ../data/drugddi2013/test.ddi
 
--- find_error.py
+++ find_error.py
@@ -26,6 +26,20 @@ def postprocessing(pred, pem, bot, keep=True):
     
     pred_copy = copy.deepcopy(pred)
     for features, tgs in tqdm(pred_copy):
+        
+        '''
+        all_ngrams = data_utils.generate_ngrams(features, nums=range(6))
+        for single_ngrams in all_ngrams:
+            for i, ngram in enumerate(single_ngrams):
+                ngram_concat = '_'.join(ngram)
+                if ngram_concat in pem:
+                    top_entity, _ = pem[ngram_concat][0]
+                    title = top_entity.replace('_', ' ')
+                    if bot.search_in_page(['DrugBank_Ref', 'ATC_prefix'], title):
+                        tgs[i:i+len(ngram)] = ['B-DRUG'] + ['I-DRUG']*(len(ngram) - 1)
+                            
+                            
+        '''                    
         for i in range(len(features)):
             if 'DRUG' in tgs[i]:
                 if features[i] in pem:
@@ -35,7 +49,7 @@ def postprocessing(pred, pem, bot, keep=True):
                         tgs[i] = 'O'
                 elif not keep:
                     tgs[i] = 'O'
-    
+        
     return pred_copy
 
 
@@ -57,14 +71,14 @@ filename = os.path.join(data_path, 'test.ddi')
 with open(filename, 'r') as f:
     gold = data_utils.iob2etype(f.readlines(), iob=True)
 
-data_utils.find_error(gold, pred, os.path.join(data_path, 'error.txt'), bot=bot, pem=pem)
+#data_utils.find_error(gold, pred, os.path.join(data_path, 'error.txt'), bot=bot, pem=pem)
 
 
 # postprocessing
 pred_pp_keep = postprocessing(pred, pem, bot, keep=True)
 pred_pp_nonkeep = postprocessing(pred, pem, bot, keep=False)
-data_utils.find_error(gold, pred_pp_keep, os.path.join(data_path, 'error_pp_keep.txt'), bot=bot, pem=pem)
-data_utils.find_error(gold, pred_pp_nonkeep, os.path.join(data_path, 'error_pp_nonkeep.txt'), bot=bot, pem=pem)
+#data_utils.find_error(gold, pred_pp_keep, os.path.join(data_path, 'error_pp_keep.txt'), bot=bot, pem=pem)
+#data_utils.find_error(gold, pred_pp_nonkeep, os.path.join(data_path, 'error_pp_nonkeep.txt'), bot=bot, pem=pem)
 
 
 # evaluation
